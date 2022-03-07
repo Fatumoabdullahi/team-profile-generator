0cf5391cdbd24137bde530f1bc0ea0b17c9dad86
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-03-07 23:47:21 +0000

    fixed

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -11,27 +11,27 @@ test('creates an employee object', () => {
 });
 // gets id from getId() 
 test('gets employee name', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('Fatumo', 12, 'fatumoabdullahi@outlook.com');
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
 // gets id from getId() 
 test('gets employee ID', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('Fatumo', 12, 'fatumoabdullahi@outlook.com');
 
     expect(employee.getId()).toEqual(expect.any(Number));
 });
 // gets emails from getEmail()
 test('gets employee email', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('Fatumo', 12, 'fatumoabdullahi@outlook.com');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const employee = new Employee('Fatumo', 12, 'fatumoabdullahi@outlook.com');
 
     expect(employee.getRole()).toEqual("Employee");
 }); 
