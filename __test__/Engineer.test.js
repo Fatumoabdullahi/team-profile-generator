dc34d44832b6e1b74683b26e37165dd0033ffab4
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-03-07 23:49:58 +0000

    engineer test

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -3,7 +3,20 @@ const Engineer = require('../lib/Engineer');
 
 // creating engineer object  
 test('creates an Engineer object', () => {
-    const engineer = new Engineer('Nicole', 90, 'nicole.elisaw@gmail', 'nicolewallace09');
+    const engineer = new Engineer('Fatumo', 23, 'fatumoabdullahi@outlook.com', 'fatumoabdullahi');
     
     expect(engineer.github) .toEqual(expect.any(String));
+});
+// gets github from getGithub()
+test('gets engineer github value', () => {
+    const engineer = new Engineer('Fatumo', 23, 'fatumoabdullahi@outlook.com', 'fatumoabdullahi');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// gets role from getRole() 
+test('gets role of employee', () => {
+    const engineer = new Engineer('Fatumo', 23, 'fatumoabdullahi@outlook.com', 'fatumoabdullahi');
+
+    expect(engineer.getRole()).toEqual("Engineer");
 });
